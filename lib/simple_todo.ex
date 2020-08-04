b9757a52509a0e497e923ec68250d84b923efc42
Beth Jackson <Mbeth.ashton@gmail.com>
2020-08-04 11:13:02 -0400

    Add in previous work

--- lib/simple_todo.ex
+++ lib/simple_todo.ex
@@ -1,10 +1,22 @@
-defmodule TodoList do
-  def new() do
+defmodule Todo do
+  def new(), do: MultiDict.new()
+  
+  def add_entry(todo_list, date, title) do
+    MultiDict.add(todo_list, date, title)
   end
-
-  def add_entry(_, _, _) do
+  
+  def entries(todo_list, date) do
+    MultiDict.get(todo_list, date)
   end
+end
 
-  def entries(_, _) do
+defmodule MultiDict do
+  def new(), do: %{}
+  def add(dict, key, value) do
+    Map.update(dict, key, [value], &[value | &1])
+  end
+  
+  def get(dict, key) do
+    Map.get(dict, key, [])
   end
 end
