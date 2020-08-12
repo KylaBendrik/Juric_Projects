497e9af2d8409c1fa1fe39ad3f1464764702925b
Beth Jackson <Mbeth.ashton@gmail.com>
2020-08-12 15:28:10 -0400

    Making sure I'm on the right page!

--- lib/simple_todo.ex
+++ lib/simple_todo.ex
@@ -21,38 +21,36 @@ defmodule TodoList do
     )
   end
   
-  def add_entry(todo_server, new_entry) do
-    send(todo_server, {:add_entry, new_entry})
+  def add_entry(todo_list, entry) do
+    entry = Map.put(entry, :id, todo_list.auto_id)
+    new_entries = Map.put(todo_list.entries, todo_list.auto_id, entry)
+
+    %TodoList{todo_list | entries: new_entries, auto_id: todo_list.auto_id + 1}
   end
-  
-  defp process_message(todo_list, {:add_entry, new_entry}) do
-    TodoList.add_entry(todo_list, new_entry)
-  end
-  
+
   def entries(todo_list, date) do
     todo_list.entries
     |> Stream.filter(fn {_, entry} -> entry.date == date end)
     |> Enum.map(fn {_, entry} -> entry end)
   end
   
+  def update_entry(todo_list, %{} = new_entry) do
+    update_entry(todo_list, new_entry.id, fn _ -> new_entry end)
+  end
+
   def update_entry(todo_list, entry_id, updater_fun) do
     case Map.fetch(todo_list.entries, entry_id) do
-      :error -> todo_list
+      :error ->
+        todo_list
+
       {:ok, old_entry} ->
-        old_entry_id = old_entry.id
-        new_entry = %{id: ^old_entry_id} = updater_fun.(old_entry)
+        new_entry = updater_fun.(old_entry)
         new_entries = Map.put(todo_list.entries, new_entry.id, new_entry)
         %TodoList{todo_list | entries: new_entries}
     end
   end
-  
-  def update_entry(todo_list, %{ } = new_entry) do
-    update_entry(todo_list, new_entry.id, fn _ -> new_entry end)
-  end
-  
+
   def delete_entry(todo_list, entry_id) do
-    new_entries = Enum.filter(todo_list.entries, fn(entry) -> entry.id != entry_id end)
-    
-    %TodoList{todo_list | entries: new_entries}
+    %TodoList{todo_list | entries: Map.delete(todo_list.entries, entry_id)}
   end
 end
