d093c6587b5ed7a0ae7a4a65057e5b388edb9f47
Beth Jackson <Mbeth.ashton@gmail.com>
2020-08-19 11:03:43 -0400

    Create Interface functions

--- lib/todo_list/key_value_store.ex
+++ lib/todo_list/key_value_store.ex
@@ -1,4 +1,19 @@
 defmodule KeyValueStore do
+  
+  # interface functions
+  def start do
+    ServerProcess.start(KeyValueStore)
+  end
+  
+  def put(pid, key, value) do
+    ServerProcess.call(pid, {:put, key, value})
+  end
+  
+  def get(pid, key) do
+    ServerProcess.call(pid, {:get, key})
+  end
+  
+  # callback functions used internally by the generaic code.
   def init do
     %{}
   end
