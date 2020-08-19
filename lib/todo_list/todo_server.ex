51c9838421e2355388ec48563f7e559a59e7d3b5
Beth Jackson <Mbeth.ashton@gmail.com>
2020-08-19 11:33:05 -0400

    Make work with serverprocess

--- lib/todo_list/todo_server.ex
+++ lib/todo_list/todo_server.ex
@@ -1,55 +1,50 @@
 defmodule TodoServer do
   def start do
-    spawn(fn -> loop(TodoList.new()) end)
+    #spawn(fn -> loop(TodoList.new()) end)
+    ServerProcess.start(TodoServer)
   end
-
+  
+  def init do
+    TodoList.new()
+  end
+  
+  # actions require a pair of new functions,
+  # one interface function and one handle_call or handle_cast
+  # we have four main actions:
+  # - add_entry
+  # - entries
+  # - update_entry
+  # - delete_entry
+  
   def add_entry(todo_server, new_entry) do
-    send(todo_server, {:add_entry, new_entry})
+    ServerProcess.cast(todo_server, {:add_entry, new_entry})
   end
-
   def entries(todo_server, date) do
-    send(todo_server, {:entries, self(), date})
-
-    receive do
-      {:todo_entries, entries} -> entries
-    after
-      5000 -> {:error, :timeout}
-    end
+    ServerProcess.call(todo_server, {:entries, date})
   end
-  
   def update_entry(todo_server, entry_id, updater_fun) do
-    send(todo_server, {:update_entry, entry_id, updater_fun})
+    ServerProcess.cast(todo_server, {:update_entry, entry_id, updater_fun})
+  end
+  def delete_entry(todo_server, entry_id) do
+    ServerProcess.cast(todo_server, {:delete_entry, entry_id})
   end
   
-  def delete_entry(todo_server, entry_id) do
-    send(todo_server, {:delete_entry, entry_id})
-  end
-
-  defp loop(todo_list) do
-    new_todo_list =
-      receive do
-        message -> process_message(todo_list, message)
-      end
-
-    loop(new_todo_list)
-  end
-
-  defp process_message(todo_list, {:add_entry, new_entry}) do
+  # callback functions
+  def handle_cast({:add_entry, new_entry}, todo_list) do
     TodoList.add_entry(todo_list, new_entry)
   end
-
-  defp process_message(todo_list, {:entries, caller, date}) do
-    send(caller, {:todo_entries, TodoList.entries(todo_list, date)})
-    todo_list
-  end
   
-  defp process_message(todo_list, {:update_entry, entry_id, updater_fun}) do
+  def handle_cast({:update_entry, entry_id, updater_fun}, todo_list) do
     TodoList.update_entry(todo_list, entry_id, updater_fun)
   end
   
-  defp process_message(todo_list, {:delete_entry, entry_id}) do
+  def handle_cast({:delete_entry, entry_id}, todo_list) do
     TodoList.delete_entry(todo_list, entry_id)
   end
+  
+  def handle_call({:entries, date}, todo_list) do
+    TodoList.entries(todo_list, date)
+  end
+  
 
-  defp process_message(todo_list, _), do: todo_list
 end
